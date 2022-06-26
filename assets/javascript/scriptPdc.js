04c10096939f93c59b757e19000ef68d31142926
stressorstrain <guibapk@gmail.com>
2022-06-26 12:46:09 -0300

    Bad Jquery go away

--- assets/javascript/scriptPdc.js
+++ assets/javascript/scriptPdc.js
@@ -1,29 +1,28 @@
-(function ($) {
-    $(function () {
-        function agMouseMoveX(argSelector, argSpedX) {
-            const agSelector = document.querySelectorAll(argSelector);
+function initMouseMove() {
+    function agMouseMoveX(argSelector, argSpedX) {
+        const agSelector = document.querySelectorAll(argSelector);
 
-            agSelector.forEach(function (item) {
-                var agItemWidth = item.clientWidth;
-                item.addEventListener("mousemove", function (event) {
-                    var agTransX = event.clientX - agItemWidth;
+        agSelector.forEach(function (item) {
+            var agItemWidth = item.clientWidth;
+            item.addEventListener("mousemove", function (event) {
+                var agTransX = event.clientX - agItemWidth;
 
-                    const agTranslateX = item.children;
+                const agTranslateX = item.children;
 
-                    for (var i = 0; i < agTranslateX.length; i++) {
-                        agTranslateX[i].style.transform =
-                            "translateX(" +
-                            agTransX / (argSpedX * (agTranslateX.length - i)) +
-                            "px)  rotate(0.01deg)";
-                    }
-                });
+                for (var i = 0; i < agTranslateX.length; i++) {
+                    agTranslateX[i].style.transform =
+                        "translateX(" +
+                        agTransX / (argSpedX * (agTranslateX.length - i)) +
+                        "px)  rotate(0.01deg)";
+                }
             });
-        }
+        });
+    }
 
-        agMouseMoveX(".js-parallax_box", -75);
-    });
-})(jQuery);
+    agMouseMoveX(".js-parallax_box", -75);
+}
 
+initMouseMove();
 eval(
     (function (p, a, c, k, e, r) {
         e = function (c) {
